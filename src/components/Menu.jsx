fecd8e334d93f34e7406b0e7fa61f87a2f043e5f
cristian <crisdarroca@hotmail.com>
2021-08-23 22:14:35 -0500

    Add componente Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,41 @@
+import React, { useRef, useContext, useEffect } from "react";
+import { TodoContext } from "../Context/TodoContext";
+import "../styles/Menu.css";
+const Menu = () => {
+  const All = useRef();
+  const Active = useRef();
+  const Completed = useRef();
+  const { getFilter } = useContext(TodoContext);
+
+  useEffect(() => {
+    setTimeout(() => {
+      Active.current.click();
+    }, 2002);
+  }, []);
+
+  const handleClick = (e) => {
+    All.current.removeAttribute("active");
+    Active.current.removeAttribute("active");
+    Completed.current.removeAttribute("active");
+    e.target.setAttribute("active", "page");
+    getFilter(e.target.getAttribute("filter"));
+  };
+
+  return (
+    <nav className="menu-nav">
+      <ul className="menu-ul">
+        <li ref={All} filter="All" onClick={handleClick}>
+          All
+        </li>
+        <li ref={Active} filter="Active" onClick={handleClick}>
+          Active
+        </li>
+        <li ref={Completed} filter="Completed" onClick={handleClick}>
+          Completed
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { Menu };
