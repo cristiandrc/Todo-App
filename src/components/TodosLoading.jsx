c8f0c9bf02b2c8bb6e7f027f9a0fb131acdf857c
cristian <crisdarroca@hotmail.com>
2021-08-25 20:58:10 -0500

    add iconos a TodoLoading

--- src/components/TodosLoading.jsx
+++ src/components/TodosLoading.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "../styles/TodosLoading.css";
+import { MdDelete, MdCheck } from "react-icons/md";
 
 function TodosLoading() {
   return (
     <div className="LoadingTodo-container">
-      <span className="LoadingTodo-completeIcon">ᄼ</span>
+      <span className="LoadingTodo-completeIcon">
+        <MdCheck />
+      </span>
       <p className="LoadingTodo-text">Cargando TODOs...</p>
-      <span className="LoadingTodo-deleteIcon">X</span>
+      <span className="LoadingTodo-deleteIcon">
+        <MdDelete />
+      </span>
     </div>
   );
 }
