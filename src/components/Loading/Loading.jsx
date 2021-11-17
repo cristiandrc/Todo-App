08fabe3752a561840ccede824a87aae2908d91cb
cristian <crisdarroca@hotmail.com>
2021-11-16 19:30:28 -0500

    add loading y error  en login

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import { Modal } from "../Modal";
+import "./styles.css";
+
+const Loading = () => {
+  return (
+    <Modal>
+      <div className="container-loading"></div>
+    </Modal>
+  );
+};
+
+export default Loading;
